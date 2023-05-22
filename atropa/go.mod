f3ef83cefd8c0ffe7447fdc8c2a62ec99e10d602
omussell <oliver.mussell@hotmail.co.uk>
2023-05-22 12:41:07 +0100

    Pebble+challtestsrv setup and testing. Go app init.

---
+++ atropa/go.mod
@@ -0,0 +1,3 @@
+module github.com/omussell/atropa
+
+go 1.20
---
+++ atropa/main.go
@@ -0,0 +1,7 @@
+# send request to cert provider
+# cert validation
+# send cert to web servers
+
+
+
+need container with nginx + atropa for serving the .well-known text files and otherwise sending to atropa.
